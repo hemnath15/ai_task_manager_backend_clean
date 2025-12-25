860e279215f9ef83bf56ddb4fca14e8b981081e4
hemnath15 <hemihari15@gmail.com>
2025-12-25 18:11:34 +0530

    final check

--- src/server.ts
+++ src/server.ts
@@ -8,6 +8,9 @@ connectDB();
 
 // Start Server
 const PORT = process.env.PORT || 5000;
+app.get("/", (req, res) => {
+  res.status(200).json({ status: "OK" });
+});
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
